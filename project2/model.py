a8eda72786da7627b48300000754756020bea980
emirferzan <emirferzan142@gmail.com>
2025-07-09 19:38:05 -0500

    build 2 models and tested to ensure they are properly built

--- project2/model.py
+++ project2/model.py
@@ -1,1 +1,49 @@
 import tensorflow as tf
+from tensorflow.keras import layers, models, regularizers
+
+def create_model_one(input_shape=(32,32,3), num_classes=100):
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=input_shape))
+    model.add(layers.MaxPooling2D((2, 2)))
+    
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+
+    model.add(layers.Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(num_classes, activation='softmax'))
+    
+    print("model built")
+    return model
+
+def build_model_two(input_shape=(32,32,3), num_classes=100):
+    model = models.Sequential()
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=input_shape))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+
+    model.add(layers.Dense(256, activation='relu', kernel_regularizer=regularizers.l2(0.001)))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(num_classes, activation='softmax'))
+
+    print("model built")
+
+    return model
+
+num_classes = 100
+input_shape = (32, 32, 3)
+
+print("Testing build_model_1:")
+model1 = create_model_one(input_shape=input_shape, num_classes=num_classes)
+
+
+print("\nTesting build_model_2:")
+model2 = build_model_two(input_shape=input_shape, num_classes=num_classes)
+
+
